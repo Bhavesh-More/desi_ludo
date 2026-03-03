a6fa5db1d698d2c9d5080108f3e63c17efa4011d
Bhavesh-More <bhaveshmmore2006@gmail.com>
2026-03-03 23:58:35 +0530

    added base client and created server folder

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return(
+    <div className="app-container">
+      <h1 style={{color:"white"}} >Welcome to Desi Ludo</h1>
+    </div>
+  )
+}
+
+export default App
