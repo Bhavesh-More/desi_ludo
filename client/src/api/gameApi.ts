04dd8026f1fba23d264c60c08655ba4e59a37f39
Bhavesh-More <bhaveshmmore2006@gmail.com>
2026-04-30 01:20:06 +0530

    add env var for API base URL

--- client/src/api/gameApi.ts
+++ client/src/api/gameApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { type GameSnapshot, type ShellFace } from "../types/gameTypes"
 
-const API_BASE = "http://localhost:8080"
+const API_BASE = import.meta.env.VITE_API_URL ?? "http://localhost:8080"
 
 export interface CreateSessionResponse {
 	sessionId: string
